d3032159a1ac711ad75d5059f9e3c9809ddfbaf6
Mkhotamirais <mkhotamirais@gmail.com>
2024-09-19 15:34:45 +0700

    err3

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 import express from "express";
-import http from "http";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
-import mongoose from "mongoose";
 
 import v1Router from "./v1/router";
 
@@ -19,18 +17,12 @@ app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
 
-const server = http.createServer(app);
-
 app.get("/", (req, res) => {
   res.send("hello");
 });
 
 app.use("/v1", v1Router);
 
-server.listen(8080, () => {
+app.listen(8080, () => {
   console.log("Server is running on http://localhost:8080");
 });
-
-mongoose.Promise = Promise;
-mongoose.connect(MONGO_URL).then(() => console.log("MongoDB connected"));
-mongoose.connection.on("error", (error: Error) => console.log(error));
