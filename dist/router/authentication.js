c5dfe8dfbb9678ecac82cd9defc7d432c59cf811
Mkhotamirais <mkhotamirais@gmail.com>
2024-09-19 16:41:55 +0700

    err6

--- dist/router/authentication.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (router) => {
-    router.post("/auth/register", (req, res) => {
-        return res.send("halo lagi");
-    });
-};
-//# sourceMappingURL=authentication.js.map
--- dist/src/v1/router.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const productController_1 = require("./productController");
-const router = (0, express_1.Router)();
-router.get("/product", productController_1.readProducts);
-exports.default = router;
-//# sourceMappingURL=router.js.map
